0921153260c3b38e69daff8eb08e75bf390aea3f
d-szymanski <mrszymeq@gmail.com>
2019-10-26 09:01:20 +0200

    feat: second type of loop

--- sum.asm
+++ sum.asm
@@ -8,7 +8,8 @@
 
 section .data
     lpCount dq  15
-    sum dq  0
+    sum1 dq  0
+    sum2 dq  0
 
 section .text
     global _start
@@ -16,17 +17,25 @@ section .text
 _start:
     mov rcx, qword [lpCount]
     mov rax, 1
-    call _odd_sum_loop
-
+    call _first_sum_loop
+    mov rax, 1
+    mov ecx, dword[lpCount]
+    call _second_sum_loop
 _end: 
   mov rax, 60
   mov rdi, 0
   syscall
 
-_odd_sum_loop:
-    add qword[sum], rax
+_first_sum_loop:
+    add qword[sum1], rax
     add rax, 2 ; move to next odd number
     dec rcx
     cmp rcx, 0
-    jne _odd_sum_loop
+    jne _first_sum_loop
     ret ; return to call
+
+_second_sum_loop:
+    add qword[sum2], rax
+    add rax, 2
+    loop _second_sum_loop
+    ret
