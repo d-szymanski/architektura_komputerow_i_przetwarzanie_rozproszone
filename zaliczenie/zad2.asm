7efdbf5ec20dbda041edf8f25beff5d261b88a4e
dawid szymanski <mrszymeq@gmail.com>
2020-01-11 14:04:34 +0100

    zad 2

--- zaliczenie/zad2.asm
+++ zaliczenie/zad2.asm
@@ -1,29 +1,43 @@
 section .data
 	fileName	db	"plik.txt"
+	len		dq	18
 
 section .bss
-	message	resb	255
+	message	resb	18
+	fd	resb	1
 
 section .text
 	global _start
 
 _start: 
 
-
+_open:
+	mov rax, 2
+	mov rdi, fileName
+	mov rsi, 0
+	mov rdx, 0644o
+	syscall
+	mov [fd], rax
 
 _read:
 	mov rax, 0
-   	mov rdi, [fd_in]
-	mov rsi, info
+   	mov rdi, [fd]
+	mov rsi, message
    	mov rdx, len
+	syscall
+
+_close:
+	mov rax, 3
+	mov rdi, [fd]
 	syscall
 
 _print:
 	mov rax, 1
 	mov rdi, 1
     	mov rsi, message
-    	mov rdx, 12
+    	mov rdx, len
 	syscall
+
 _end:
         mov rax, 60
         mov rdi, 0
