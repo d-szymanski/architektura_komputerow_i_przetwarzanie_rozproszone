608729e11ee2ddfe9bca63a09441b0bec6e070d7
dawid szymanski <mrszymeq@gmail.com>
2020-01-11 10:53:17 +0100

    feat: lab3 zad 1

---
+++ lab3/zad1.asm
@@ -0,0 +1,41 @@
+section .data
+	arr1	dq	1, 2, 3, 4, 5
+	arr2	dq	5, 6, 7, 8, 9
+
+section .bss
+	arr3	resq	5
+	result	resq	1
+
+section .text
+	global _start
+
+_start:
+	mov ecx, 5
+	mov rbx, 0
+
+_sumTwoValues:
+	mov rax, qword[arr1 + rbx * 8]
+	add rax, qword[arr2 + rbx * 8]
+	mov qword[arr3 + rbx * 8], rax
+	push rax
+	inc rbx
+	loop _sumTwoValues
+
+_beforeSumAll:
+	mov ecx, 5
+	mov rbx, 0
+	mov rax, 0
+
+_sumAll:
+	pop rsi
+	add rax, rsi
+	inc rbx
+	loop _sumAll
+
+_writeResult
+	mov qword[result], rax
+
+_end:
+        mov rax, 60
+        mov rdi, 0
+        syscall
